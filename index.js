e8d98c770df827e48bad997c45d19327f670a209
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-08 18:28:28 -0400

    Adds package-lock.json

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+
+}
+function subtract(a, b) {
+    return a - b;
+
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b
+
+}
+function increment(n) { 
+    n++
+    return n 
+}
+function decrement(n) {
+    n--
+    return n
+}
+function makeInt(string) {
+    return parseInt(string, 10); 
+    // return string
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+    // return string
+
+}
